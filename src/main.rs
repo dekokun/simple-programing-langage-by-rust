69e5789e0cbbeaaa888cc3c0a3141075afe40732
dekokun <dekokun@hatena.ne.jp>
2018-02-15 01:01:10 +0900

    エラー処理

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::process;
 
 fn main() {
     let program = env::args().nth(1).expect("Missing argument");
@@ -12,10 +13,19 @@ fn eval(program: String, mut pointer: usize) -> usize {
     }
     let p = program.chars().nth(pointer).unwrap();
     pointer += 1;
-    let mut val = p.to_digit(10).unwrap();
-    while pointer <= program.len() - 1 && program.chars().nth(pointer).unwrap().is_digit(10) {
-        val = val * 10 + program.chars().nth(pointer).unwrap().to_digit(10).unwrap();
-        pointer += 1;
+    if p.is_digit(10) {
+        let mut val = p.to_digit(10).unwrap();
+        while pointer <= program.len() - 1 && program.chars().nth(pointer).unwrap().is_digit(10) {
+            val = val * 10 + program.chars().nth(pointer).unwrap().to_digit(10).unwrap();
+            pointer += 1;
+        }
+        return val as usize;
     }
-    return val as usize;
+    error(format!("Invalid character: {:?}", p));
+    return 0;
+}
+
+fn error(error: String) {
+    eprintln!("{}", error);
+    process::exit(1);
 }
