302d3b0ad0720a1bd3f9f58ebf4fa08b726eda6d
dekokun <dekokun@hatena.ne.jp>
2018-02-15 00:36:25 +0900

    数値を返す

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 use std::env;
 
 fn main() {
-    let p = env::args().nth(1).expect("Missing argument");
+    let program = env::args().nth(1).expect("Missing argument");
 
-    println!("{:?}", eval(p, 0));
+    println!("{:?}", eval(program, 0));
 }
 
-fn eval(p: String, pointer: usize) -> usize {
-    if pointer == p.len() {
+fn eval(program: String, mut pointer: usize) -> usize {
+    let p = program.chars().nth(pointer).unwrap();
+    pointer += 1;
+    if p.is_digit(10) {
+        let mut val = p.to_digit(10).unwrap();
+        while pointer <= program.len() - 1 && program.chars().nth(pointer).unwrap().is_digit(10) {
+            val = val * 10 + program.chars().nth(pointer).unwrap().to_digit(10).unwrap();
+            pointer += 1;
+        }
+        return val as usize;
+    }
+    if pointer == program.len() - 1 {
         return 0;
     }
-    return eval(p, pointer + 1);
+    return eval(program, pointer);
 }
